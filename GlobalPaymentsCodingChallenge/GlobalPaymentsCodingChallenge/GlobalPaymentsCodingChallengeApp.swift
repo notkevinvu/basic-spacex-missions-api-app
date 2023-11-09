9bef3133d0fe36ccba5e4e496a4a6f412688fbc5
Kevin Vu <kevinvu59@gmail.com>
2023-11-09 11:44:05 -0500

    Initial project setup

---
+++ GlobalPaymentsCodingChallenge/GlobalPaymentsCodingChallenge/GlobalPaymentsCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalPaymentsCodingChallengeApp.swift
+//  GlobalPaymentsCodingChallenge
+//
+//  Created by Kevin Vu on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct GlobalPaymentsCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
