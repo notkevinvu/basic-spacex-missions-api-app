26f6c4b1d9f682de91e7a2a590566b5a4f373b06
Kevin Vu <kevinvu59@gmail.com>
2023-11-09 13:25:14 -0500

    Basic hookup of view to vm to call getLaunches

--- GlobalPaymentsCodingChallenge/GlobalPaymentsCodingChallenge/ContentView.swift
+++ GlobalPaymentsCodingChallenge/GlobalPaymentsCodingChallenge/ContentView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var viewModel = LaunchListViewModel()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+        }
+        .task {
+            await viewModel.getLaunches()
         }
         .padding()
     }
